16d80d6d2b64f5f0534e10d05f37076c2f6ae7c4
amansavani <savaniaman@gmail.com>
2020-10-15 11:25:38 -0700

    alerts

--- src/app/detailspage/detailspage.component.ts
+++ src/app/detailspage/detailspage.component.ts
@@ -3,6 +3,14 @@ import { AutocompService } from '../services/autocomp.service';
 import { FormControl } from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
 @Component({
   selector: 'app-detailspage',
   templateUrl: './detailspage.component.html',
@@ -14,16 +22,36 @@ export class DetailspageComponent implements OnInit {
   dailypriceObj:object;
   metadataObj:object;
   format: string = "";
+  totalPrice: string="0.00";
   change:number;
   changePercent:number;
   timestamp:string;
   marketOpen:boolean=true;
   closeResult:string;
   isStarred:boolean=false;
-  constructor(private router:Router, private route:ActivatedRoute, private _autocompservice:AutocompService, private modalService: NgbModal) { }
+  numStocks:number;
 
+  // alerts start
+  
 
+  alerts: Alert[];
+  close(alert: Alert) {
+
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  
+  //alerts end
+  constructor(private router:Router, private route:ActivatedRoute, private _autocompservice:AutocompService, private modalService: NgbModal) {this.alerts=[];}
+
+  calculateCost(x){
+    this.numStocks=x;
+    this.totalPrice=(x*this.dailypriceObj["last"].toFixed(2)).toString();
+    // console.log(this.totalPrice);
+  }
+  
   open(content) {
+    this.totalPrice="0.00";
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
@@ -40,9 +68,37 @@ export class DetailspageComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
+  buyStock(){
+    let new_alert:Alert={type:"success",message:this.metadataObj["ticker"]+" bought successfully!"};
+    this.alerts.unshift(new_alert);
+    setTimeout(this.close.bind(this),3000,new_alert);
+    console.log("must buy "+this.metadataObj["ticker"]+" at rate "+this.dailypriceObj["last"]+" quantity "+this.numStocks+" total cost "+this.totalPrice);
+    
+    
+    
+    this.numStocks=0;
+  }
 
   toggle(){
     this.isStarred = !this.isStarred;
+    
+    // this._success.next(this.metadataObj["ticker"]+ " added to Watchlist.");
+    // this._success.subscribe(message => this.successMessage = message);
+    // this._success.pipe(
+    //   debounceTime(5000)
+    // ).subscribe(() => this.successMessage = '');
+    let new_alert:Alert;
+    if (this.isStarred){
+      // alert:Alert = {type:"success",message:this.metadataObj["ticker"]+" added to Watchlist."};
+      new_alert={type:"success",message:this.metadataObj["ticker"]+" added to Watchlist."}
+      
+    }
+    else{
+      new_alert={type:"danger",message:this.metadataObj["ticker"]+" removed from Watchlist."};
+    }
+    this.alerts.unshift(new_alert);
+    setTimeout(this.close.bind(this),3000,new_alert);
+
     // must save data
   }
   ngOnInit(): void {
@@ -56,7 +112,7 @@ export class DetailspageComponent implements OnInit {
       this.marketOpen=true;
       this.timestamp=date.getFullYear() + "-" + String((date.getMonth() + 1)).padStart(2, '0') + "-" + String(date.getDate()).padStart(2, '0')+" "+String(date.getHours()).padStart(2,'0')+":"+String(date.getMinutes()).padStart(2,'0')+":"+String(date.getSeconds()).padStart(2,'0');
       // console.log(this.changePercent);
-      if (this.dailypriceObj["bidPrice"]==null && this.dailypriceObj["bidSize"]==null){
+      if (this.dailypriceObj["bidPrice"]==null && this.dailypriceObj["bidSize"]==null && this.dailypriceObj["askSize"]==null && this.dailypriceObj["askPrice"]==null){
         this.format = this.dailypriceObj["timestamp"].slice(0,10)+" "+this.dailypriceObj["timestamp"].slice(11,19)
         this.marketOpen=false;
       }
